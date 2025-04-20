ad2b56d0286c2d549316a5e36f350688b7580acc
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-04-20 20:26:39 +0530

    js

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-const express = require('express')
-
-const port=3000;
-
+const express = require('express');
 const app = express();
-app.use(express.json());
+
+const port = 3000;
+app.use(express.json())
 
 const users = [
     { email: "john@example.com", password: "john123" },
-    { email: "jane@example.com", password: "jane123" },
-    { email: "bob@example.com", password: "bob123" },
+    { email: "jane@example.com", password: "jane123" }
   ];
-
-
-app.post('/login', (req, res)=>{
+  
+app.put('/update', (req, res)=>{
     const {email, password} = req.body;
-    const user = users.find(user => user.email === email && user.password === password);
-    if (!email) {
-        return res.status(400).json({ message: "Email cannot be empty" });
-      }
-     if (!password){
-        return res.status(400).json({ message: "Password cannot be empty" });
-      }
-
-    if(user && user.password===password){
-        res.json({message: "Login successful", user});
+    if(!email){
+        res.status(400).send({message: "Email is required"});
     }
-    else{
-        res.status(401).json({message: "Invalid email or password"});
-        }
-});
+    if(!password){
+        res.status(400).send({message: "Password is required"});
+    }
+    const user = users.find(user => user.email === email);
+    if (user) {
+        user.password = password;
+        res.json({message: "Password updated successfully"});
+        } else {
+            res.status(404).json({message: "User not found"});
+            }
+
+})
+
+app.delete('/delete', (req, res)=>{
+    const {email} = req.body;
+    if(!email){
+        res.status(400).send({message: "Email is required"});
+    }
+    const user = users.find(user => user.email === email);
+    if (user) {
+        const index = users.indexOf(user);
+        users.splice(index, 1);
+        res.json({message: "User deleted successfully"});
+        } else {
+            res.status(404).json({message: "User not found"});
+            }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    });
+});
